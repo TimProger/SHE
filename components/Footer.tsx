188efdda0bbfead226df5b9fadc77b669a7d530a
TimProger <akylverentimofei@mail.ru>
2022-11-20 03:36:13 +0300

    Футер со стилями

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import styles from '../styles/components/footer.module.scss'
 import React from "react";
 import {useRouter} from "next/router";
+import Link from "next/link";
 
 interface IFooterProps {
   links: string[];
@@ -13,49 +14,91 @@ const Footer: React.FC<IFooterProps> = ({links, titles}) => {
 
   return (
     <footer className={styles.footer}>
-
-      <table>
-        <thead>
-        <tr>
-          <th>{titles[0]}</th>
-          <th>{titles[1]}</th>
-          <th>{titles[2]}</th>
-          <th>{titles[3]}</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>{links[0]}</td>
-          <td>{links[4]}</td>
-          <td>{links[9]}</td>
-          <td>{links[12]}</td>
-        </tr>
-        <tr>
-          <td>{links[1]}</td>
-          <td>{links[5]}</td>
-          <td>{links[10]}</td>
-          <td>{links[13]}</td>
-        </tr>
-        <tr>
-          <td>{links[2]}</td>
-          <td>{links[6]}</td>
-          <td>{links[11]}</td>
-          <td>{links[14]}</td>
-        </tr>
-        <tr>
-          <td>{links[3]}</td>
-          <td>{links[7]}</td>
-          <td></td>
-          <td>{links[15]}</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>{links[8]}</td>
-          <td></td>
-          <td></td>
-        </tr>
-        </tbody>
-      </table>
+      <div className={styles.wrapper}>
+        <div className={styles.footer__img}></div>
+        <table className={styles.footer__table}>
+          <thead>
+          <tr>
+            <th>{titles[0]}</th>
+            <th>{titles[1]}</th>
+            <th>{titles[2]}</th>
+            <th>{titles[3]}</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>
+              <Link href={'/profile'}>{links[0]}</Link>
+            </td>
+            <td>
+              <Link href={'/info'}>{links[4]}</Link>
+            </td>
+            <td>
+              <Link href={'/contacts'}>{links[9]}</Link>
+            </td>
+            <td>
+              <Link href={'/video'}>{links[12]}</Link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Link href={'/profile'}>{links[1]}</Link>
+            </td>
+            <td>
+              <Link href={'/profile'}>{links[5]}</Link>
+            </td>
+            <td>
+              <Link href={'/profile'}>{links[10]}</Link>
+            </td>
+            <td>
+              <Link href={'/profile'}>{links[13]}</Link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Link href={'/profile'}>{links[2]}</Link>
+            </td>
+            <td>
+              <Link href={'/profile'}>{links[6]}</Link>
+            </td>
+            <td>
+              <Link href={'/profile'}>{links[11]}</Link>
+            </td>
+            <td>
+              <Link href={'/profile'}>{links[14]}</Link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Link href={'/profile'}>{links[3]}</Link>
+            </td>
+            <td>
+              <Link href={'/profile'}>{links[7]}</Link>
+            </td>
+            <td>
+              <Link href={'/profile'}></Link>
+            </td>
+            <td>
+              <Link href={'/profile'}>{links[15]}</Link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Link href={'/profile'}></Link>
+            </td>
+            <td>
+              <Link href={'/profile'}>{links[8]}</Link>
+            </td>
+            <td>
+              <Link href={'/profile'}></Link>
+            </td>
+            <td>
+              <Link href={'/profile'}></Link>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     </footer>
   )
 }
