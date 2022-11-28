4a66eed60023f91881e470c8c4684dfcc0c332e1
Timofei Akylveren <akylverentimofei@mail.ru>
2022-11-28 16:53:45 +0300

    Корзина готова.

--- components/CardFloat.tsx
+++ components/CardFloat.tsx
@@ -127,9 +127,10 @@ const CardFloat: React.FC<ICardProps> = ({product, isBasket = false}) => {
           <div onClick={removeFromBasketHandler}>-</div>
           {product?.count}
           <div onClick={addToBasketHandler}>+</div>
-        </div> : <svg className={s.card__price__basket} width="18" height="24" viewBox="0 0 18 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path className={isInBasket ? s.card__price__basket__active : ''} d="M5 9V5C5 2.79 6.795 1 9 1C11.21 1 13 2.795 13 5V9M1 7H17V23H1V7Z" stroke="#A0A0A0" strokeLinecap="round"/>
-        </svg>
+        </div> : ''
+        // <svg className={s.card__price__basket} width="18" height="24" viewBox="0 0 18 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        //   <path className={isInBasket ? s.card__price__basket__active : ''} d="M5 9V5C5 2.79 6.795 1 9 1C11.21 1 13 2.795 13 5V9M1 7H17V23H1V7Z" stroke="#A0A0A0" strokeLinecap="round"/>
+        // </svg>
         }
       </div>
     </div>
