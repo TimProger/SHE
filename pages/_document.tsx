b34255ea5196e52daadb5cebe03793a1ab380afb
Timofei Akylveren <akylverentimofei@mail.ru>
2022-12-03 00:54:05 +0300

    почти починил корзину и прикрутил лимит блоков в каталоге

---
+++ pages/_document.tsx
@@ -0,0 +1,49 @@
+import React, {FC, useEffect} from 'react';
+import {AppProps} from 'next/app';
+import {wrapper} from "../store";
+import '../styles/globals.scss'
+import {appWithTranslation} from "next-i18next";
+import {Storage} from "../utils/storage";
+import {useAppDispatch} from "../hooks/useTypedDispatch";
+import {getFavs} from "../store/ActionCreators/Fav.ac";
+import {useRouter} from "next/router";
+import {useTypedSelector} from "../hooks/useTypedSelector";
+import {getUser} from "../store/ActionCreators/Profile.ac";
+import {getBasket} from "../store/ActionCreators/Basket.ac";
+
+const WrappedApp: FC<AppProps> = ({Component, pageProps}) => {
+
+  const { locale } = useRouter()
+  const dispatch = useAppDispatch()
+  const {isLoading} = useTypedSelector(state => state.fav)
+  const profileState = useTypedSelector(state => state.profile)
+
+  useEffect(()=>{
+    if(!isLoading){
+      // Проверить на isAuth и если true, то не заходить в Storage
+      // и сразу отправлять запрос за favs и basket
+      const favs = Storage.get('favs')
+      if(favs && JSON.parse(favs).length > 0){
+        dispatch(getFavs({ids: JSON.parse(favs), locale}))
+      }
+    }
+  }, [])
+
+  useEffect(()=>{
+    if(!profileState.isAuth){
+      if(Storage.get('accessToken')){
+        dispatch(getUser())
+      }
+    }
+  },[])
+
+  useEffect(()=>{
+    if(profileState.isAuth){
+      dispatch(getBasket(locale || 'ru'))
+    }
+  }, [profileState.isAuth])
+
+  return <Component {...pageProps} />
+};
+
+export default wrapper.withRedux(appWithTranslation(WrappedApp));
